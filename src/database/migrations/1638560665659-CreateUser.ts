7cc18a819735fc73c7f7f0256111b939b36040f1
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-03 17:33:42 -0300

    chore: create migration to create users

---
+++ src/database/migrations/1638560665659-CreateUser.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUser1638560665659 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'age',
+            type: 'int',
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+          },
+          {
+            name: 'weight',
+            type: 'int',
+          },
+          {
+            name: 'ethnicity',
+            type: 'enum',
+            enum: ['branco', 'pardo', 'preto', 'amarelo', 'indigena'],
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
