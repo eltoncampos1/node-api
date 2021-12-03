15515329999823cfe321c8eb30b31222bcd7b4b9
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-03 19:29:33 -0300

    feat: create error handling

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import './database';
 import 'shared/container';
-import express from 'express';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 
+import { AppError } from 'errors/errors';
 import { router } from './routes';
 
 const { PORT } = process.env;
@@ -12,5 +14,18 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 app.listen(PORT, () => console.log(`app is running on::${PORT}`));
