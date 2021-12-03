66db43917b23fadd6dcc823c8d3f14eb50014e6b
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-03 19:44:30 -0300

    test: should not be able to create an user if already exists

--- src/modules/users/use-cases/create-user.spec.ts
+++ src/modules/users/use-cases/create-user.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../errors/errors';
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 import { UsersRepositoryInMemory } from '../repositories/im-memory/UsersRepositoryInMemory';
 import { CreateUserUseCase } from './create-user';
@@ -23,5 +24,31 @@ describe('Create User', () => {
     await createUserUseCase.execute(user);
 
     expect(usersRepository.users[0]).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with an existing email', async () => {
+    const user1: ICreateUserDTO = {
+      name: 'user',
+      phone: 'user_phone',
+      email: 'user@email.com',
+      age: 20,
+      weight: 60,
+      ethnicity: 'branco',
+    };
+
+    const user2: ICreateUserDTO = {
+      name: 'user2',
+      phone: 'user2_phone',
+      email: 'user@email.com',
+      age: 20,
+      weight: 60,
+      ethnicity: 'branco',
+    };
+
+    await createUserUseCase.execute(user1);
+
+    expect(async () => {
+      await createUserUseCase.execute(user2);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
