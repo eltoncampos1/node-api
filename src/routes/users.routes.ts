13904f0cacb2d7924c30a1e5bf8e7941f9ea5599
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-03 17:32:54 -0300

    chore: create routes

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { CreateUserController } from 'modules/users/use-cases/create-user-controller';
+
+const usersRoutes = Router();
+
+const createUserController = new CreateUserController();
+
+usersRoutes.post('/create', createUserController.handle);
+
+export { usersRoutes };
