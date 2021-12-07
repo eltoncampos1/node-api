ff6d9a7b3389b57a6960217c254c6407ca075ad7
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-06 22:47:23 -0300

    chore: add contrac to repository to create an user session

--- src/modules/users/repositories/IUsersRepositories.ts
+++ src/modules/users/repositories/IUsersRepositories.ts
@@ -4,6 +4,9 @@ import { User } from '../entities/User';
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User | undefined>;
+  passwordHash(password: string): Promise<string>;
+  comparePassword(password: string, user_password: string): Promise<boolean>;
+  generateToken(user: User): Promise<string>;
 }
 
 export { IUsersRepository };
