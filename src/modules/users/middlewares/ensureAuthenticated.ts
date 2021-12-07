899d032cfe48c91d4fd823aa60802bb4acb0a092
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-07 18:36:07 -0300

    feat: middleware to verify if user is authenticated

---
+++ src/modules/users/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,30 @@
+import { AppError } from 'errors/errors';
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, process.env.JWT_HASH as string);
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+  } catch {
+    throw new AppError('Invalid Token!', 401);
+  }
+}
