dfdf5355c2640fe18618b5ed7cb52e0b984e34f3
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-07 20:45:25 -0300

    chore: add mod findbyId and save to userRepository contract

--- src/modules/users/repositories/IUsersRepositories.ts
+++ src/modules/users/repositories/IUsersRepositories.ts
@@ -5,6 +5,8 @@ interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   list(): Promise<User[]>;
   findByEmail(email: string): Promise<User | undefined>;
+  findByID(user_id: string): Promise<User | undefined>;
+  save(user: User): Promise<void>;
 }
 
 export { IUsersRepository };
