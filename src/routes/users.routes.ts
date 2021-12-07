520b399b17e6a65b8d1bd9cf628ec0e91c3e32ef
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-07 18:24:12 -0300

    chore: implement route to list all users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { ListUsersController } from 'modules/users/use-cases/list-users/list-users-controller';
 import { CreateUserController } from '../modules/users/use-cases/create-user/create-user-controller';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const listUserController = new ListUsersController();
 
 usersRoutes.post('/create', createUserController.handle);
+usersRoutes.get('/list', listUserController.handle);
 
 export { usersRoutes };
