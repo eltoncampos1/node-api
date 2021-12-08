99f65bd2a646a58acb4f40ea490db8c42ed42186
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-07 23:15:52 -0300

    test: should be able to delete an user

---
+++ src/modules/users/use-cases/delete-user/delete-user.spec.ts
@@ -0,0 +1,36 @@
+import { ICreateUserDTO } from 'modules/users/dtos/ICreateUserDTO';
+import FakeHashProvider from 'modules/users/providers/HashProvider/fake/fakeHashProvider';
+import { UsersRepositoryInMemory } from 'modules/users/repositories/im-memory/UsersRepositoryInMemory';
+import { CreateUserUseCase } from '../create-user/create-user';
+import { DeleteUserUseCase } from './delete-user';
+
+let usersRepository: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+let deleteUserUseCase: DeleteUserUseCase;
+let hashProvider: FakeHashProvider;
+describe('Delete User', () => {
+  beforeEach(() => {
+    usersRepository = new UsersRepositoryInMemory();
+    hashProvider = new FakeHashProvider();
+    createUserUseCase = new CreateUserUseCase(usersRepository, hashProvider);
+    deleteUserUseCase = new DeleteUserUseCase(usersRepository);
+  });
+
+  it('should be able to delete an  user', async () => {
+    const user: ICreateUserDTO = {
+      name: 'user',
+      phone: 'user_phone',
+      password: '1234',
+      email: 'user@email.com',
+      age: 20,
+      weight: 60,
+      ethnicity: 'branco',
+    };
+
+    await createUserUseCase.execute(user);
+
+    await deleteUserUseCase.execute(usersRepository.users[0].id);
+
+    expect(usersRepository.users.length).toBe(0);
+  });
+});
