c92b23e4897d9017922bbc2fbd0878bdf0cffb5a
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-07 23:14:45 -0300

    chore: add delete user contract

--- src/modules/users/repositories/IUsersRepositories.ts
+++ src/modules/users/repositories/IUsersRepositories.ts
@@ -7,6 +7,7 @@ interface IUsersRepository {
   findByEmail(email: string): Promise<User | undefined>;
   findByID(user_id: string): Promise<User | undefined>;
   save(user: User): Promise<void>;
+  delete(user_id: string): Promise<void>;
 }
 
 export { IUsersRepository };
