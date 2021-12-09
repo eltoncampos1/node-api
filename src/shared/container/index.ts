d43c23af0827b77b318d3e9f409b5df942ecc2c1
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-09 13:08:23 -0300

    chore: register addresss repository instance

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,5 @@
+import { IAddressRepository } from 'modules/address/repositories/IAddressRepository';
+import { AddressPostgresRepository } from 'modules/address/repositories/implementations/AddressPostgresRepository';
 import { BCryptHashProvider } from 'modules/users/providers/HashProvider/implementations/BCryptHashProvider';
 import { IHashProvider } from 'modules/users/providers/HashProvider/models/IHashProvider';
 import { PostgresUsersRepository } from 'modules/users/repositories/implementations/PostegresUsersRepository';
@@ -5,5 +7,5 @@ import { IUsersRepository } from 'modules/users/repositories/IUsersRepositories'
 import { container } from 'tsyringe';
 
 container.registerSingleton<IUsersRepository>('PostgresUsersRepository', PostgresUsersRepository);
-
 container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
+container.registerSingleton<IAddressRepository>('AddressRepository', AddressPostgresRepository);
