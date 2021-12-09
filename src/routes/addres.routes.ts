322e9ee0c40348c8e2569a49d6307a78cd275fe8
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-09 16:52:14 -0300

    feat: list user Address route

--- src/routes/addres.routes.ts
+++ src/routes/addres.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { CreateAddressController } from 'modules/address/use-cases/create-address/create-address-controller';
+import { ListUserAddressControler } from 'modules/address/use-cases/list-user-address/list-user-address-controller';
 import ensureAuthenticated from 'modules/users/middlewares/ensureAuthenticated';
 
 const addressRoutes = Router();
 const createAddressController = new CreateAddressController();
+const listUserAddressController = new ListUserAddressControler();
 
 addressRoutes.post('/create', ensureAuthenticated, createAddressController.handle);
+addressRoutes.get('/', ensureAuthenticated, listUserAddressController.handle);
 
 export { addressRoutes };
